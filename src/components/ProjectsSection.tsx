0f52a12c96b1bd21d08c9bbc2feeff7ca3dece9e
Maxwell paulo <maxpaulo.mps@gmail.com>
2025-06-07 14:32:21 -0300

    Add 'Sertanejo Madeiras' project to ProjectsSection and localization updates

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -24,6 +24,7 @@ const projectItems = [
     { id: "theInteriorEdge", href: "https://theinterioredge-sa.com/", imageSrc: "https://xl7064gpfgvoyo7p.public.blob.vercel-storage.com/the-interior-edge-home-5YkhwWvST8jKIP77GZaSnxHfkZcZUq", width: 1200, height: 630 },
     { id: "coffeeShopTiaRosa", href: "https://coffee-shop-tia-rosa-seven.vercel.app/", imageSrc: "https://xl7064gpfgvoyo7p.public.blob.vercel-storage.com/coffe-shop-tia-rosa-home-4cipUJ4Jr4nH62OMwFbTaRXi8yxOit", width: 1200, height: 630 },
     { id: "personalPortfolio", href: "https://personal-portfolio-eight-pied.vercel.app/", imageSrc: "https://xl7064gpfgvoyo7p.public.blob.vercel-storage.com/personal-portfolio-home-wIwEvjrRc2GiSv55s8du7qh99JIOyw", width: 1200, height: 630 },
+    { id: "sertanejoMadeiras", href: "https://sertanejomadeiras.com.br/", imageSrc: "https://xl7064gpfgvoyo7p.public.blob.vercel-storage.com/sertanejo-madeiras-home-ReB8Oz41Ndzo15dX574SuAOuCL31H0", width: 1200, height: 630 },
 ];
 
 export function ProjectsSection() {
