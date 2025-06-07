43f18cef61d7b58563cf0e28af6c3640f0d66eb6
Maxwell paulo <maxpaulo.mps@gmail.com>
2025-06-07 14:29:41 -0300

    Update ProjectsSection and localization for new projects
    - Replaced 'Project 3' and 'Project 4' with 'Coffee Shops Tia Rosa' and 'Personal Portfolio' in the ProjectsSection component, including updated images and links.
    - Enhanced localization files in English and Portuguese to include detailed descriptions for the new projects, showcasing their features and technologies used.

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -22,8 +22,8 @@ import Image from 'next/image';
 const projectItems = [
     { id: "finestFlights", href: "https://finestflights.com/", imageSrc: "https://xl7064gpfgvoyo7p.public.blob.vercel-storage.com/finest-flights-home-iFUS4UaShmlbYHYht6CLK8Hm77cSju", width: 1200, height: 630 },
     { id: "theInteriorEdge", href: "https://theinterioredge-sa.com/", imageSrc: "https://xl7064gpfgvoyo7p.public.blob.vercel-storage.com/the-interior-edge-home-5YkhwWvST8jKIP77GZaSnxHfkZcZUq", width: 1200, height: 630 },
-    { id: "project3", href: "/projects/project3", imageSrc: "https://via.placeholder.com/400x200?text=Project+3", width: 400, height: 200 },
-    { id: "project4", href: "/projects/project4", imageSrc: "https://via.placeholder.com/400x200?text=Project+4", width: 400, height: 200 },
+    { id: "coffeeShopTiaRosa", href: "https://coffee-shop-tia-rosa-seven.vercel.app/", imageSrc: "https://xl7064gpfgvoyo7p.public.blob.vercel-storage.com/coffe-shop-tia-rosa-home-4cipUJ4Jr4nH62OMwFbTaRXi8yxOit", width: 1200, height: 630 },
+    { id: "personalPortfolio", href: "https://personal-portfolio-eight-pied.vercel.app/", imageSrc: "https://xl7064gpfgvoyo7p.public.blob.vercel-storage.com/personal-portfolio-home-wIwEvjrRc2GiSv55s8du7qh99JIOyw", width: 1200, height: 630 },
 ];
 
 export function ProjectsSection() {
