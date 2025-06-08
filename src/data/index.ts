df6df8ca69f840e67b101cd2d3862a82da53fc23
Maxwell paulo <maxpaulo.mps@gmail.com>
2025-06-08 12:29:54 -0300

    Refactor project items organization by moving data to a new file
    - Transferred project items from index.ts to a new projects.ts file for improved organization and maintainability.
    - Updated index.ts to export the new projects module, enhancing clarity in data structure.

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,11 +1,4 @@
-export const projectItems = [
-    { id: "finestFlights", href: "https://finestflights.com/", imageSrc: "https://xl7064gpfgvoyo7p.public.blob.vercel-storage.com/finest-flights-home-iFUS4UaShmlbYHYht6CLK8Hm77cSju", width: 1200, height: 630 },
-    { id: "theInteriorEdge", href: "https://theinterioredge-sa.com/", imageSrc: "https://xl7064gpfgvoyo7p.public.blob.vercel-storage.com/the-interior-edge-home-5YkhwWvST8jKIP77GZaSnxHfkZcZUq", width: 1200, height: 630 },
-    { id: "coffeeShopTiaRosa", href: "https://coffee-shop-tia-rosa-seven.vercel.app/", imageSrc: "https://xl7064gpfgvoyo7p.public.blob.vercel-storage.com/coffe-shop-tia-rosa-home-4cipUJ4Jr4nH62OMwFbTaRXi8yxOit", width: 1200, height: 630 },
-    { id: "personalPortfolio", href: "https://personal-portfolio-eight-pied.vercel.app/", imageSrc: "https://xl7064gpfgvoyo7p.public.blob.vercel-storage.com/personal-portfolio-home-wIwEvjrRc2GiSv55s8du7qh99JIOyw", width: 1200, height: 630 },
-    { id: "sertanejoMadeiras", href: "https://sertanejomadeiras.com.br/", imageSrc: "https://xl7064gpfgvoyo7p.public.blob.vercel-storage.com/sertanejo-madeiras-home-ReB8Oz41Ndzo15dX574SuAOuCL31H0", width: 1200, height: 630 },
-];
-
+export * from './projects';
 export * from './technologies';
 export * from './experience';
 export * from './education';
